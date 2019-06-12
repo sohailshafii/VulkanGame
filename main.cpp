db3f4360d8af818263865b0229d898955c71f709
Sohail Shafii <sohail.shafii@gmail.com>
2019-06-12 07:02:14 -0700

    start writing code for depth buffer

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@
 #include <array>
 
 #define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
@@ -26,7 +27,7 @@
 #include "stb_image.h"
 
 struct Vertex {
-	glm::vec2 pos;
+	glm::vec3 pos;
 	glm::vec3 color;
 	glm::vec2 texCoord;
 
@@ -42,7 +43,7 @@ struct Vertex {
 		std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions = {};
 		attributeDescriptions[0].binding = 0;
 		attributeDescriptions[0].location = 0;
-		attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+		attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
 		attributeDescriptions[0].offset = offsetof(Vertex, pos);
 
 		attributeDescriptions[1].binding = 0;
@@ -82,14 +83,20 @@ private:
 	const int HEIGHT = 600;
 
 	const std::vector<Vertex> vertices = {
-		{{-0.5f,-0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-		{ { 0.5f,-0.5f },{ 0.0f, 1.0f, 0.0f }, {0.0f, 0.0f} },
-		{ {0.5f, 0.5f },{ 0.0f, 0.0f, 1.0f }, {0.0f, 1.0f} },
-		{{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f}}
+		{{-0.5f,-0.5f, 0.0f }, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+		{ { 0.5f,-0.5f, 0.0f },{ 0.0f, 1.0f, 0.0f }, {0.0f, 0.0f} },
+		{ {0.5f, 0.5f, 0.0f },{ 0.0f, 0.0f, 1.0f }, {0.0f, 1.0f} },
+		{{-0.5f, 0.5f, 0.0f }, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f}},
+
+		{ { -0.5f,-0.5f,-0.5f },{ 1.0f, 0.0f, 0.0f },{ 1.0f, 0.0f } },
+		{ { 0.5f,-0.5f,-0.5f },{ 0.0f, 1.0f, 0.0f },{ 0.0f, 0.0f } },
+		{ { 0.5f, 0.5f,-0.5f },{ 0.0f, 0.0f, 1.0f },{ 0.0f, 1.0f } },
+		{ { -0.5f, 0.5f,-0.5f },{ 1.0f, 1.0f, 1.0f },{ 1.0f, 1.0f } }
 	};
 
 	const std::vector<uint16_t> indices = {
-		0, 1, 2, 2, 3, 0
+		0, 1, 2, 2, 3, 0,
+		4, 5, 6, 6, 7, 4
 	};
 	const int MAX_FRAMES_IN_FLIGHT = 2;
 
