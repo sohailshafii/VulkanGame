515469cf99c9377164a79155448b0f287e82b8d8
Sohail Shafii <sohail.shafii@gmail.com>
2019-09-12 20:47:05 -0700

    add image data to class

--- CommonImage.h
+++ CommonImage.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include "vulkan/vulkan.h"
+
 // Represents common Image and the view onto it, etc.
 class CommonImage {
 public:
 	CommonImage();
 	~CommonImage();
+
+private:
+	VkImage image;
+	VkImageView imageView;
 };
