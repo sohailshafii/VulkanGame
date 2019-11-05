fc7a347d1271187e2d8f683b49d7f6c0be0d8280
Sohail Shafii <sohail.shafii@gmail.com>
2019-11-04 21:43:45 -0800

    add swap chain manager to graphics engine class

--- GraphicsEngine.cpp
+++ GraphicsEngine.cpp
@@ -1,6 +1,28 @@
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 #include "GraphicsEngine.h"
+#include "SwapChainManager.h"
 
 GraphicsEngine::GraphicsEngine() {
 
 }
+
+void GraphicsEngine::CreateEngine(GfxDeviceManager* gfxDeviceManager,
+	LogicalDeviceManager* logicalDeviceManager, VkSurfaceKHR surface,
+	GLFWwindow* window) {
+	swapChainManager = new SwapChainManager(gfxDeviceManager,
+		logicalDeviceManager);
+	swapChainManager->create(surface, window);
+}
+
+GraphicsEngine::~GraphicsEngine() {
+	CleanUpSwapChain();
+}
+
+void GraphicsEngine::CleanUpSwapChain() {
+	if (swapChainManager != nullptr) {
+		delete swapChainManager;
+	}
+}
+ 
--- GraphicsEngine.h
+++ GraphicsEngine.h
@@ -1,8 +1,24 @@
 #pragma once
 
+#include "vulkan/vulkan.h"
+
+class SwapChainManager;
+class GfxDeviceManager;
+class LogicalDeviceManager;
+struct GLFWwindow;
+
 class GraphicsEngine {
 public:
 	GraphicsEngine();
 
+	void CreateEngine(GfxDeviceManager* gfxDeviceManager,
+		LogicalDeviceManager* logicalDeviceManager, VkSurfaceKHR surface,
+		GLFWwindow* window);
+
+	~GraphicsEngine();
+
 private:
+	SwapChainManager* swapChainManager;
+
+	void CleanUpSwapChain();
 };
