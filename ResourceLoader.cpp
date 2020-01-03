a7c7e575d7d5bda59e1a0149cc6983086455b569
Sohail Shafii <sohail.shafii@gmail.com>
2020-01-02 22:22:29 -0800

    add a bit more functionality to resource loader

--- ResourceLoader.cpp
+++ ResourceLoader.cpp
@@ -1,3 +1,15 @@
 
 #include "ResourceLoader.h"
 
+ResourceLoader::ResourceLoader() {
+
+}
+
+ResourceLoader::~ResourceLoader() {
+
+}
+
+
+/*ShaderModule& ResourceLoader::getShader(std::string path) {
+	return ShaderModule("TODO", ); // TODO
+}*/
--- ResourceLoader.h
+++ ResourceLoader.h
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <map>
+#include <string>
+#include "ShaderModule.h"
+
 // Responsible for loading and caching any
 // resources used, like textures, shaders,
 // sounds, et cetera.
 class ResourceLoader {
+public:
+	ResourceLoader();
+	~ResourceLoader();
 
+	//ShaderModule& getShader(std::string path);
+
+private:
+
+	std::map<std::string, ShaderModule> shadersLoaded;
 };
