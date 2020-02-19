e78a3af5919717f72255661320106c8241ddbd19
Sohail Shafii <sohail.shafii@gmail.com>
2020-02-18 22:32:54 -0800

    remove commented out code

--- GraphicsEngine.cpp
+++ GraphicsEngine.cpp
@@ -29,10 +29,7 @@ GraphicsEngine::GraphicsEngine(GfxDeviceManager* gfxDeviceManager,
 	CreateDepthResources(gfxDeviceManager, commandPool); // 6
 	CreateFramebuffers(); // 7
 	CreateUniformBuffers(gfxDeviceManager, gameObjects); // 8
-
-	/*CreateDescriptorPool();
-	CreateDescriptorSets(descriptorSetLayout, imageTexture->getTextureImageView(),
-		imageTexture->getTextureImageSampler(), gameObjects);*/
+	
 	CreateDescriptorPoolAndSets(descriptorSetLayout, imageTexture->GetTextureImageView(), imageTexture->GetTextureImageSampler(), gameObjects);
 	CreateCommandBuffers(commandPool, gameObjects);
 }
@@ -42,7 +39,7 @@ GraphicsEngine::~GraphicsEngine() {
 }
 
 void GraphicsEngine::CleanUpSwapChain() {
-	// TODO: verify notnull throughout
+	// TODO: verify not null throughout
 	for (size_t i = 0; i < swapChainFramebuffers.size(); i++) {
 		vkDestroyFramebuffer(logicalDeviceManager->GetDevice(), swapChainFramebuffers[i],
 			nullptr);
@@ -164,83 +161,6 @@ void GraphicsEngine::CreateUniformBuffers(GfxDeviceManager* gfxDeviceManager,
 		gameObject->CreateCommandBuffers(gfxDeviceManager, numSwapChainImages);
 	}
 }
-
-/*void GraphicsEngine::CreateDescriptorPool() {
-	std::array<VkDescriptorPoolSize, 2> poolSizes = {};
-	poolSizes[0].type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-	const std::vector<VkImage>& swapChainImages = swapChainManager->getSwapChainImages();
-	poolSizes[0].descriptorCount = static_cast<uint32_t>(swapChainImages.size());
-	poolSizes[1].type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-	poolSizes[1].descriptorCount = static_cast<uint32_t>(swapChainImages.size());
-
-	VkDescriptorPoolCreateInfo poolInfo = {};
-	poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
-	poolInfo.poolSizeCount = static_cast<uint32_t>(poolSizes.size());
-	poolInfo.pPoolSizes = poolSizes.data();
-	poolInfo.maxSets = static_cast<uint32_t>(swapChainImages.size());
-
-	if (vkCreateDescriptorPool(logicalDeviceManager->getDevice(), &poolInfo,
-		nullptr, &descriptorPool) != VK_SUCCESS) {
-		throw std::runtime_error("failed to create descriptor pool!");
-	}
-}*/
-
-// ubo -> descriptor set -> draw calls
-// each ubo has its own descriptor set.
-// for each object, we have N ubos, and each ubo has an associated descriptor set
-// we need a way to allow multiple ubos, which means multiple descriptor sets, each of which is associated with a game object. so associate a descriptor set with a game object
-/*void GraphicsEngine::CreateDescriptorSets(VkDescriptorSetLayout descriptorSetLayout,
-	VkImageView textureImageView, VkSampler textureSampler,
-	std::vector<std::shared_ptr<GameObject>>& gameObjects) {
-	const std::vector<VkImage>& swapChainImages = swapChainManager->getSwapChainImages();
-	std::vector<VkDescriptorSetLayout> layouts(swapChainImages.size(),
-		descriptorSetLayout);
-	VkDescriptorSetAllocateInfo allocInfo = {};
-	allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
-	allocInfo.descriptorPool = descriptorPool;
-	allocInfo.descriptorSetCount = static_cast<uint32_t>(swapChainImages.size());
-	allocInfo.pSetLayouts = layouts.data();
-
-	descriptorSets.resize(swapChainImages.size());
-	if (vkAllocateDescriptorSets(logicalDeviceManager->getDevice(), &allocInfo,
-		descriptorSets.data()) != VK_SUCCESS) {
-		throw std::runtime_error("Failed to allocate descriptor sets!");
-	}
-	
-	for (size_t i = 0; i < swapChainImages.size(); ++i) {
-		VkDescriptorBufferInfo bufferInfo = {};
-		// TODO: do this per game object?
-		bufferInfo.buffer = gameObjects[0]->GetUniformBuffer(i);
-		bufferInfo.offset = 0;
-		bufferInfo.range = sizeof(UniformBufferObject);
-
-		VkDescriptorImageInfo imageInfo = {};
-		imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-		imageInfo.imageView = textureImageView;
-		imageInfo.sampler = textureSampler;
-
-		std::array<VkWriteDescriptorSet, 2> descriptorWrites = {};
-		descriptorWrites[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-		descriptorWrites[0].dstSet = descriptorSets[i];
-		descriptorWrites[0].dstBinding = 0;
-		descriptorWrites[0].dstArrayElement = 0;
-		descriptorWrites[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-		descriptorWrites[0].descriptorCount = 1;
-		descriptorWrites[0].pBufferInfo = &bufferInfo;
-
-		descriptorWrites[1].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-		descriptorWrites[1].dstSet = descriptorSets[i];
-		descriptorWrites[1].dstBinding = 1;
-		descriptorWrites[1].dstArrayElement = 0;
-		descriptorWrites[1].descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-		descriptorWrites[1].descriptorCount = 1;
-		descriptorWrites[1].pImageInfo = &imageInfo;
-
-		vkUpdateDescriptorSets(logicalDeviceManager->getDevice(), static_cast<uint32_t>(descriptorWrites.size()),
-			descriptorWrites.data(), 0,
-			nullptr);
-	}
-}*/
 
 void GraphicsEngine::CreateDescriptorPoolAndSets(VkDescriptorSetLayout descriptorSetLayout,
 								 VkImageView textureImageView, VkSampler textureSampler,
