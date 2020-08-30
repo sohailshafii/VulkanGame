aeea6d05014e9900d7e6d6cb2dca7702a3b1d386
Sohail Shafii <sohail.shafii@gmail.com>
2020-08-29 18:26:01 -0700

    add code to reach pawn

--- src/GameObjects/PawnBehavior.cpp
+++ src/GameObjects/PawnBehavior.cpp
@@ -4,6 +4,7 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/gtc/matrix_transform.hpp>
 #include <cmath>
+#include <iostream>
 
 const float PawnBehavior::acceleration = 0.1f;
 const float PawnBehavior::maxVelocityMagnitude = 2.0f;
@@ -26,46 +27,56 @@ GameObjectBehavior::BehaviorStatus PawnBehavior::UpdateSelf(float time,
 		return GameObjectBehavior::BehaviorStatus::Normal;
 	}
 
-	auto returnVal = GameObjectBehavior::BehaviorStatus::Normal;
 	auto playerGameObject = scene->GetPlayerGameObject();
-	if (playerGameObject != nullptr) {
-		auto playerWorldPosition =
-			playerGameObject->GetWorldPosition();
-		glm::vec3 pawnPosition = GetWorldPosition();
-		auto vectorToPlayer = playerWorldPosition -
-			pawnPosition;
-		glm::normalize(vectorToPlayer);
-		// figure out which side of the plane we are on
-		// assume player is on plane, normal vector to player
-		float planeDistance = -glm::dot(vectorToPlayer, playerWorldPosition);
-		float pawnPositionPlaneDist = glm::dot(vectorToPlayer,
-			pawnPosition) + planeDistance;
-		float planeDistSignBefore = pawnPositionPlaneDist < 0.0f ? -1.0f : 1.0f;
-
-		// semi-implicit euler
-		currentVelocity += acceleration * deltaTime;
-		if (currentVelocity > maxVelocityMagnitude) {
-			currentVelocity = maxVelocityMagnitude;
-		}
-		else if (currentVelocity < -maxVelocityMagnitude) {
-			currentVelocity = -maxVelocityMagnitude;
-		}
-		pawnPosition += currentVelocity * vectorToPlayer;
-		// did we overshoot? i.e. did we go on other side of plane
-		float pawnPositionPlaneDistAfter = glm::dot(vectorToPlayer,
-			pawnPosition) + planeDistance;
-		// we went on other side of plane if signs of distances differ
-		float planeDistSignAfter = pawnPositionPlaneDistAfter < 0.0f ?
-			-1.0f : 1.0f;
-		if (planeDistSignBefore * planeDistSignAfter < 0.0f) {
-			pawnPosition = playerWorldPosition;
-			returnVal = GameObjectBehavior::BehaviorStatus::Destroyed;
-		}
-
-		modelMatrix[3][0] = pawnPosition[0];
-		modelMatrix[3][1] = pawnPosition[1];
-		modelMatrix[3][2] = pawnPosition[2];
+	if (playerGameObject == nullptr) {
+		return GameObjectBehavior::BehaviorStatus::Normal;
 	}
 
-	return returnVal;
+	// TODO: remove test code
+	auto playerWorldPosition =
+		playerGameObject->GetWorldPosition() - glm::vec3(0.0f, 0.0f, 3.0f);
+	glm::vec3 pawnPosition = GetWorldPosition();
+	auto vectorToPlayer = playerWorldPosition -
+		pawnPosition;
+
+	if (glm::length(vectorToPlayer) < 0.00f) {
+		std::cout << "close to pawn already!\n";
+		return GameObjectBehavior::BehaviorStatus::Destroyed;
+	}
+
+	glm::normalize(vectorToPlayer);
+	// figure out which side of the plane we are on
+	// assume player is on plane, normal vector to player
+	float planeDistance = -glm::dot(vectorToPlayer, playerWorldPosition);
+	float pawnPositionPlaneDist = glm::dot(vectorToPlayer,
+		pawnPosition) + planeDistance;
+	float planeDistSignBefore = pawnPositionPlaneDist < 0.0f ?
+		-1.0f : 1.0f;
+
+	// semi-implicit euler
+	currentVelocity += acceleration * deltaTime;
+	if (currentVelocity > maxVelocityMagnitude) {
+		currentVelocity = maxVelocityMagnitude;
+	}
+	else if (currentVelocity < -maxVelocityMagnitude) {
+		currentVelocity = -maxVelocityMagnitude;
+	}
+	pawnPosition += currentVelocity * vectorToPlayer;
+	// did we overshoot? i.e. did we go on other side of plane
+	float pawnPositionPlaneDistAfter = glm::dot(vectorToPlayer,
+		pawnPosition) + planeDistance;
+	// we went on other side of plane if signs of distances differ
+	float planeDistSignAfter = pawnPositionPlaneDistAfter < 0.0f ?
+		-1.0f : 1.0f;
+	if (planeDistSignBefore * planeDistSignAfter < 0.0f ||
+		fabs(planeDistSignAfter) < 0.00001f) {
+		pawnPosition = playerWorldPosition;
+		std::cout << "reached pawn now!\n";
+	}
+
+	modelMatrix[3][0] = pawnPosition[0];
+	modelMatrix[3][1] = pawnPosition[1];
+	modelMatrix[3][2] = pawnPosition[2];
+
+	return GameObjectBehavior::BehaviorStatus::Normal;
 }
