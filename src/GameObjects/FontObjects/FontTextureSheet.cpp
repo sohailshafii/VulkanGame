68938081228d55f6702ccd71479f3283205e9b7e
Sohail Shafii <sohail.shafii@gmail.com>
2020-12-10 22:30:07 -0800

    start writing font loading logic

--- src/GameObjects/FontObjects/FontTextureSheet.cpp
+++ src/GameObjects/FontObjects/FontTextureSheet.cpp
@@ -5,16 +5,25 @@
 #include <string>
 
 FontTextureSheet::FontTextureSheet() {
-	InitFreeTypeLibrary();
+	FT_Library freeTypeLibrary = InitFreeTypeLibrary();
+
+	if (freeTypeLibrary != nullptr) {
+		BuildFonts(freeTypeLibrary);
+
+		FT_Done_FreeType(freeTypeLibrary);
+	}
 }
 
-void FontTextureSheet::InitFreeTypeLibrary() {
-	FT_Library ft;
-	if (FT_Init_FreeType(&ft)) {
+FT_Library FontTextureSheet::InitFreeTypeLibrary() {
+	FT_Library freeTypeLibrary;
+	if (FT_Init_FreeType(&freeTypeLibrary)) {
 		std::cerr << "FontTextureSheet: Could not init the FreeType Library.\n";
-		return;
+		return nullptr;
 	}
+	return freeTypeLibrary;
+}
 
+void FontTextureSheet::BuildFonts(FT_Library freeTypeLibrary) {
 #if __APPLE__
 	const std::fontPath fontPathPrefix = "../../fonts/";
 #else
@@ -23,11 +32,30 @@ void FontTextureSheet::InitFreeTypeLibrary() {
 	const std::string fontPath = fontPathPrefix + "oxanium/Oxanium-Medium.ttf";
 
 	FT_Face face;
-	if (FT_New_Face(ft, fontPath.c_str(), 0, &face)) {
+	if (FT_New_Face(freeTypeLibrary, fontPath.c_str(), 0, &face)) {
 		std::cerr << "FontTextureSheet: Failed to load font.\n";
+		return;
 	}
 
 	FT_Set_Pixel_Sizes(face, 0, 48);
+	std::vector<FontRasterInfo> fontRasterInfos;
+	for (unsigned char c = 0; c < 128; c++)
+	{
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+			std::cerr << "FontTextureSheet: Failed to load glyph!\n";
+			continue;
+		}
+
+		auto glyph = face->glyph;
+		auto& bitmap = glyph->bitmap;
+		fontRasterInfos.push_back(FontRasterInfo(bitmap.width,
+			bitmap.rows, bitmap.pitch, bitmap.buffer));
+
+		fontPositioningInfos.push_back(FontPositioningInfo(bitmap.width,
+			bitmap.rows, glyph->bitmap_left, glyph->bitmap_top));
+	}
+
+	FT_Done_Face(face);
 
 	std::cout << "Successfully loaded font.\n";
 }
--- src/GameObjects/FontObjects/FontTextureSheet.h
+++ src/GameObjects/FontObjects/FontTextureSheet.h
@@ -1,9 +1,73 @@
 #pragma once
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include <vector>
+
 class FontTextureSheet {
 public:
 	FontTextureSheet();
 private:
+	struct FontRasterInfo {
+		FontRasterInfo() : rows(0), width(0),
+			pitch(0), buffer(nullptr) {
+		}
 
-	void InitFreeTypeLibrary();
+		FontRasterInfo(FontRasterInfo const &other) {
+			this->rows = other.rows;
+			this->width = other.width;
+			this->pitch = other.pitch;
+			int numBytes = rows * width;
+			this->buffer = new unsigned char[numBytes];
+			memcpy(this->buffer, other.buffer, numBytes);
+		}
+
+		FontRasterInfo(FontRasterInfo&& other)
+		{
+			this->rows = other.rows;
+			this->width = other.width;
+			this->pitch = other.pitch;
+			this->buffer = other.buffer;
+			other.buffer = nullptr;
+		}
+
+		FontRasterInfo(unsigned int rows, unsigned int width,
+			int pitch, unsigned char* srcBuffer) {
+			this->rows = rows;
+			this->width = width;
+			this->pitch = pitch;
+			int numBytes = rows * width;
+			this->buffer = new unsigned char[numBytes];
+			memcpy(this->buffer, buffer, numBytes);
+		}
+
+		~FontRasterInfo() {
+			if (buffer != nullptr) {
+				delete[] buffer;
+			}
+		}
+
+		unsigned int rows;
+		unsigned int width;
+		int pitch;
+		unsigned char* buffer;
+	};
+
+	struct FontPositioningInfo {
+		FontPositioningInfo(unsigned int rows,
+			unsigned int width, signed int bitMapLeft,
+			signed int bitMapRight) : rows(rows), width(width),
+			bitMapLeft(bitMapLeft), bitMapRight(bitMapRight) {
+		}
+
+		unsigned int rows;
+		unsigned int width;
+		signed int bitMapLeft;
+		signed int bitMapRight;
+	};
+
+	FT_Library InitFreeTypeLibrary();
+	void BuildFonts(FT_Library freeTypeLibrary);
+
+	std::vector<FontPositioningInfo> fontPositioningInfos;
 };
